248d1948fd7b8b8bc909fd76af8683c833d1f82f
Atomgroup530 <xul007@ucsd.edu>
2022-11-03 17:52:01 -0700

    "A2 - create an <article> element

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -11,6 +11,7 @@ class RecipeCard extends HTMLElement {
     let shadowEl = this.attachShadow({mode:'open'});
 
     // A2. TODO - Create an <article> element - This will hold our markup once our data is set
+    let elementArt = document.createElement('article');
     // A3. TODO - Create a style element - This will hold all of the styles for the Web Component
     // A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made
     // A5. TODO - Append the <style> and <article> elements to the Shadow DOM
