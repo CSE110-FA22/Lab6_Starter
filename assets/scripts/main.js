7a9ed5f51bb3093b2cf5bf66b3f9869b947162dc
Atomgroup530 <xul007@ucsd.edu>
2022-11-05 13:55:24 -0700

    B6 Create a new <recipe-card> element

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -85,6 +85,7 @@ function initFormHandler() {
       recipeObject.property = FormData.property;
     }
     // B6. TODO - Create a new <recipe-card> element
+    let recipeEl = document.createElement('recipe-card');
     // B7. TODO - Add the recipeObject data to <recipe-card> using element.data
     // B8. TODO - Append this new <recipe-card> to <main>
     // B9. TODO - Get the recipes array from localStorage, add this new recipe to it, and
