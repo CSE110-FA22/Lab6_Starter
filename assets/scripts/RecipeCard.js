3d33fd8e4bf043b04e9d4ea95114a58052cf4305
Atomgroup530 <xul007@ucsd.edu>
2022-11-05 12:18:54 -0700

    A2 A6 Delete console message

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -12,7 +12,6 @@ class RecipeCard extends HTMLElement {
 
     // A2. TODO - Create an <article> element - This will hold our markup once our data is set
     let elementArt = document.createElement('article');
-    console.log(elementArt);
     // A3. TODO - Create a style element - This will hold all of the styles for the Web Component
     let elementStyle = document.createElement('style');
 
@@ -125,7 +124,6 @@ class RecipeCard extends HTMLElement {
 
     // A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
     let shadowArt = this.shadowRoot.querySelector('article');
-    console.log(shadowArt);
     // A7. TODO - Set the contents of the <article> with the <article> template given in
     //           cardTemplate.html and the data passed in (You should only have one <article>,
     //           do not nest an <article> inside another <article>). You should use Template
