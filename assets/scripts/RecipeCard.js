a0537d74f224de0433f8315dc487b1fc30fb6ce2
Elvis Joa <ejoa@ucsd.edu>
2023-05-03 16:22:49 -0700

    Finished expose and explore

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -14,7 +14,8 @@ class RecipeCard extends HTMLElement {
     // A3. TODO - Create a style element - This will hold all of the styles for the Web Component
     var styleElem = document.createElement('style');
     // A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made
-    styleElem.appendChild(document.createTextNode(`* {
+    styleElem.appendChild(document.createTextNode(`
+    * {
       font-family: sans-serif;
       margin: 0;
       padding: 0;
